4e216cf0a0bb21009504457a892d90a95676dd0c
hamza ennaji <persona5magician@gmail.com>
2022-10-18 16:09:53 +0100

    .

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,6 @@ kubectl delete deploy backend-feed
 kubectl delete deploy reverseproxy
 kubectl delete deploy frontend
 
-kubectl apply -f feed-service.yaml
-kubectl apply -f user-service.yaml
-kubectl apply -f frontend-service.yaml
-kubectl apply -f reverseproxy-service.yaml
-
 
 kubectl apply -f feed-deploy.yml
 kubectl apply -f user-deploy.yml
